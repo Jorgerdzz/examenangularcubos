86e8b8b3629372cb6fed88216d37f1c44d3461b8
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-11-18 09:26:15 +0100

    Estructura proyecto realizada

---
+++ src/app/components/compras-component/compras-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compras-component',
+  imports: [],
+  templateUrl: './compras-component.html',
+  styleUrl: './compras-component.css',
+})
+export class ComprasComponent {
+
+}
---
+++ src/app/components/cubos-marca-component/cubos-marca-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cubos-marca-component',
+  imports: [],
+  templateUrl: './cubos-marca-component.html',
+  styleUrl: './cubos-marca-component.css',
+})
+export class CubosMarcaComponent {
+
+}
